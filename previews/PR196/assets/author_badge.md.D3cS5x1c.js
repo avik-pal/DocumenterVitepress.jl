9a2cdd0be70b10a9623c3c4ceb19aced3f0d6a97
Documenter.jl <documenter@juliadocs.github.io>
2025-01-09 22:54:01 +0000

    build based on 9241372

---
+++ previews/PR196/assets/author_badge.md.D3cS5x1c.js
@@ -0,0 +1,53 @@
+import{_ as k,c as d,j as s,a,G as l,a2 as r,w as n,B as h,o}from"./chunks/framework.Ct3KuxFt.js";const A=JSON.parse('{"title":"","description":"","frontmatter":{"authors":[{"name":"Jane Smith"},{"name":"John Doe","platform":"bluesky"},{"name":"Lazaro","avatar":"https://avatars.githubusercontent.com/u/19525261?v=4","platform":"github","link":"https://github.com/lazarusA"}]},"headers":[],"relativePath":"author_badge.md","filePath":"author_badge.md","lastUpdated":null}'),g={name:"author_badge.md"};function c(u,i,m,y,b,v){const p=h("Authors"),t=h("AuthorBadge"),e=h("PluginTabsTab"),E=h("PluginTabs");return o(),d("div",null,[i[7]||(i[7]=s("h2",{id:"badges-via-the-frontmatter",tabindex:"-1"},[a("Badges via the frontmatter "),s("a",{class:"header-anchor",href:"#badges-via-the-frontmatter","aria-label":'Permalink to "Badges via the frontmatter"'},"​")],-1)),l(p),i[8]||(i[8]=r(`<p><strong>Input</strong></p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">\`\`\`@raw html</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">---</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">authors:</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  - name: Jane Smith</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  - name: John Doe</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    platform: bluesky</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">  - name: Lazaro</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    avatar: https://avatars.githubusercontent.com/u/19525261?v=4</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    platform: github</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    link: https://github.com/lazarusA</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">---</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">## Badges via the frontmatter</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">&lt;Authors /&gt;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">\`\`\`</span></span></code></pre></div><h2 id="AuthorBadge-and-platform-icons" tabindex="-1"><code>AuthorBadge</code> and platform icons <a class="header-anchor" href="#AuthorBadge-and-platform-icons" aria-label="Permalink to &quot;\`AuthorBadge\` and platform icons {#AuthorBadge-and-platform-icons}&quot;">​</a></h2><p>You can include an <code>AuthorBadge</code> directly anywhere. See examples below.</p>`,4)),l(t,{author:"John Doe 2"}),l(t,{author:"Isaac",platform:"x"}),l(t,{author:"Marie",platform:"gitlab"}),l(t,{author:"Nikola",platform:"github"}),l(t,{author:"Galileo",platform:"linkedin"}),l(t,{author:"Ada",platform:"bluesky"}),l(t,{author:"Jane",platform:"mastodon",link:"#"}),i[9]||(i[9]=s("p",null,[s("strong",null,"Input")],-1)),l(E,null,{default:n(()=>[l(e,{label:"default"},{default:n(()=>i[0]||(i[0]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="John Doe 2"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1}),l(e,{label:"X"},{default:n(()=>i[1]||(i[1]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="Isaac"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  platform="x"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1}),l(e,{label:"gitlab"},{default:n(()=>i[2]||(i[2]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="Marie"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  platform="gitlab"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1}),l(e,{label:"github"},{default:n(()=>i[3]||(i[3]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="Nikola"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  platform="github"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1}),l(e,{label:"linkedin"},{default:n(()=>i[4]||(i[4]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="Galileo"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  platform="linkedin"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1}),l(e,{label:"bluesky"},{default:n(()=>i[5]||(i[5]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="Ada"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  platform="bluesky"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1}),l(e,{label:"mastodon"},{default:n(()=>i[6]||(i[6]=[s("div",{class:"language-md vp-adaptive-theme"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"md"),s("pre",{class:"shiki shiki-themes github-light github-dark vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```@raw html")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"<AuthorBadge")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  author="Jane"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  platform="mastodon"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},'  link="#"')]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"/>")]),a(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-light":"#24292E","--shiki-dark":"#E1E4E8"}},"```")])])])],-1)])),_:1})]),_:1})])}const B=k(g,[["render",c]]);export{A as __pageData,B as default};
